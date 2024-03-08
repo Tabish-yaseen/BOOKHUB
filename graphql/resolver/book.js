a3ff5ccf6dc011c6213bed5f4d6eb9eea6d0dbd8
Tabish-yaseen <tabishmir36@gmail.com>
2024-03-09 01:59:39 +0530

    fetching and updating the books

--- graphql/resolver/book.js
+++ graphql/resolver/book.js
@@ -1,27 +1,80 @@
-const Book = require('../../models/book');
-const { getAuthor, getGenres } = require('./helpers');
+const Book = require("../../models/book");
+const { getAuthor, getGenres } = require("./helpers");
 
 module.exports = {
+  books: async () => {
+    try {
+      const books = await Book.find();
+      if (!books || books.length === 0) {
+        return [];  
+    }
+      return books.map((book) => {
+        return {
+          ...book._doc,
+          _id: book.id,
+          publicationDate: new Date(book._doc.publicationDate).toISOString(),
+          author: getAuthor.bind(this, book.author),
+          genres: getGenres.bind(this, book.genres),
+        };
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+
   createBook: async (args) => {
     try {
       const book = new Book({
         title: args.bookInput.title,
         description: args.bookInput.description,
-        publicationDate: args.bookInput.publicationDate,
+        publicationDate: new Date(args.bookInput.publicationDate),
         author: args.bookInput.authorId,
         genres: args.bookInput.genreIds,
       });
 
       const result = await book.save();
 
+      if (!result) {
+        throw new Error('Book creation failed.');
+    }
+
       return {
         ...result._doc,
         _id: result.id,
+        publicationDate: new Date(result._doc.publicationDate).toISOString(),
         author: getAuthor.bind(this, result._doc.author),
-        genres: getGenres.bind(this, result._doc.genres), 
+        genres: getGenres.bind(this, result._doc.genres),
       };
     } catch (err) {
       throw err;
     }
   },
+
+  updateBook: async ({ id, bookInput }) => {
+    try {
+        const book = await Book.findById(id);
+
+        if (!book) {
+            throw new Error('Book not found.');
+        }
+
+        book.title = bookInput.title;
+        book.description = bookInput.description;
+        book.publicationDate = new Date(bookInput.publicationDate);
+        book.author = bookInput.authorId;
+        book.genres = bookInput.genreIds;
+
+        const updatedBook = await book.save();
+
+        return {
+            ...updatedBook._doc,
+            _id: updatedBook.id,
+            publicationDate: new Date(updatedBook._doc.publicationDate).toISOString(),
+            author: getAuthor.bind(this, updatedBook._doc.author),
+            genres: getGenres.bind(this, updatedBook._doc.genres),
+        };
+    } catch (err) {
+        throw err;
+    }
+},
 };
